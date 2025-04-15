a84995af309178ca98737cee28ba443e9935aeb8
Devin <1983029606@qq.com>
2025-04-15 18:12:04 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <n-watermark content="Devin" cross fullscreen :font-size="16" :line-height="16" :width="384" :height="384" :x-offset="12" :y-offset="50" :rotate="-15" />
+    <n-watermark content="Devin 11" cross fullscreen :font-size="16" :line-height="16" :width="384" :height="384" :x-offset="12" :y-offset="50" :rotate="-15" />
     <n-config-provider class="wh100" :theme="darkTheme">
         <n-message-provider>
             <RouterView />
